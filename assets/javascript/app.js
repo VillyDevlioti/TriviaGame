1f18f9274aa3442eb448edc637d851b841db2df9
Villy Devlioti <vadevlio@gmail.com>
2019-10-12 15:52:12 -0400

    progressing on code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,88 @@
 $(document).ready(function() {
 
 var count = 0;
+var correct = 0;
+var wrong = 0;
+
 var quizQuestions = [
     { 
-    q :"Which one of these is NOT a name of a Spice girl", 
-    answers: ["Scary Spice", "Posh Spice", "Ginger Spice", "Smelly Spice"],
-    correctAnswer: "Smelly Spice",
-    image: "/assets/images/spiceGirls.gif"
+        q :"Which one of these is NOT a name of a Spice girl", 
+        answers: ["Scary Spice", "Posh Spice", "Ginger Spice", "Smelly Spice"],
+        correctAnswer: "Smelly Spice",
+        image: "./assets/images/spiceGirls.gif"
+    },
+    {
+        q: "What is the name of Andy's bank in Parks and Rec?",
+        answers: ["Snakehole Club", "Cockroach", "Mouse Rat", "Pest Control"],
+        correctAnswer: "Mouse Rat",
+        image:"./assets/images/mouserat.gif"
     }
+
 ]
+
+function yay() {
+    correct++;
+    console.log(correct);
+    $('#answers').empty(); //clearing answers to show message
+    $('#answers').text("Yas qween!");
+    $('#image').html("<img src="+quizQuestions[count].image+">")
+    count++;
+    playGame(quizQuestions[count]);
+
+}
+
+function nay() {
+    wrong++;
+    console.log(wrong);
+    $('#answers').empty(); //clearing answers to show message
+    $('#answers').text("NOPE.");
+    $('#answers').text("The correct answer is: "+quizQuestions[count].correctAnswer);
+    $('#image').html("<img src="+quizQuestions[count].image+">");
+    count++;
+    playGame(quizQuestions[count]);
+}
 
 function playGame(quizQuestion){
    //clear button
    $('#start-here').html(""); 
+   $('#answers').empty(); //clearing previous answers
    console.log("Inside playGame");
    
    //print question
-  // $('#questions').text(quizQuestion.q);
    console.log(quizQuestion.q);
+   $('#questions').text(quizQuestion.q);
 
    //print multiple choice answers
-   $('#answers').html("<ul class=\"list-group\">");
-   
-   var secondKey = Object.keys(quizQuestion)[2]; 
-   console.log("This is the secondKey",secondKey;
-   console.log("Alternative print:", Object.keys(quizQuestion)[2].wrong2);
+   $.each(quizQuestion.answers, function(key, value ) {
+    $('#answers').append("<h2 id=\"answer-item\">"+value+"</h2>")
+    console.log(value);
+  });
 
-    for (key in quizQuestion) {
-        console.log("key: " +   quizQuestion[key]);
-        $('#questions').text(quizQuestion.q);
+   console.log("Before clicking") ;
+  //check answer
+  $("h2[id*='answer-item']").on("click", function(){
+      console.log("Checking answer", $(this).text(), "vs", quizQuestion.correctAnswer)
+    if ($(this).text() === quizQuestion.correctAnswer) {
+        yay();
+    } else {
+        nay();
     }
+  });
 
-    for (var i=0; i<4; i++);
-    {
-
-    }
 }
-
 function initializeGame () {
     console.log(count);
     if (count===0 || count === quizQuestions.length){
         console.log("Start Game");
         $('#start-here').html("<button type=\"button\" class=\"btn btn-primary btn-lg btn-block\">"+"Start here"+"</button>");
+        correct=0;
+        wrong=0;
+
     }
-    $('.btn').on("click",function(){
+    $('#start-here').on("click",function(){
         playGame(quizQuestions[count]);
     });
 }
-
 initializeGame();
 
 });
